9b0f99e47634d68eae14f2ef3f32585372cf606b
MarcelJurtz <jurtzmarcel@gmail.com>
2018-09-04 21:28:24 +0200

    Add sample content

---
+++ PopupDemo/PopupDemo/Dialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using Xamarin.Forms.Xaml;
+
+namespace PopupDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Dialog : PopupPage
+	{
+		public Dialog ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            PopupNavigation.Instance.PopAsync();
+        }
+	}
+}
